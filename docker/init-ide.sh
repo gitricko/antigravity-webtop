dc11eab099f9d1dd71543c8d8cd0c5661b1d0c52
fredrickchew <fredrick.chew@gmail.com>
2026-04-25 15:22:09 +0000

    First iteration of Antigravity and Codex

--- docker/init-ide.sh
+++ docker/init-ide.sh
@@ -2,6 +2,7 @@
 source /custom-cont-init.d/common.sh || exit 1
 
 SRC="/custom-cont-init.d/Antigravity.desktop"
-
-sync_desktop_file "$SRC" "/config/.config/autostart/Antigravity.desktop"
 sync_desktop_file "$SRC" "/config/Desktop/Antigravity.desktop"
+
+SRC="/custom-cont-init.d/Codex.desktop"
+sync_desktop_file "$SRC" "/config/Desktop/Codex.desktop"
